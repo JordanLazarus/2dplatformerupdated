6273d6b59d09e8f7816fd1acca60928cf28af240
JordanLazarus <j.j.lazarus@student.saintkentigern.com>
2020-09-25 01:18:23 +1200

    Revert "Revert "Revert "Version 2"""
    This reverts commit 7acec7563fb3cee60c2995dca99bba44eb5ef9c7.

---
+++ Goose Adventures Version 1/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Called every frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+
+
+    }
+
+}
